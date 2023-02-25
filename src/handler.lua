157b3105fcd2b094361291d581d75426df6500e9
m.garmestani <m.garmestani@sadad.co.ir>
2023-02-25 15:30:47 +0330

    set kafka log base on http log plugin

--- src/handler.lua
+++ src/handler.lua
@@ -1,6 +1,9 @@
-local basic_serializer = require "kong.plugins.kong-kafka-log.basic"
 local producers = require "kong.plugins.kong-kafka-log.producers"
+local basic_serializer = require "kong.plugins.log-serializers.basic"
+local body_transformer = require "kong.plugins.response-transformer.body_transformer"
 local cjson = require "cjson"
+local is_json_body = body_transformer.is_json_body
+local cjson_decode = cjson.decode
 local cjson_encode = cjson.encode
 local producer
 local kong = kong
@@ -9,6 +12,29 @@ local KongKafkaLogHandler = {}
 
 KongKafkaLogHandler.PRIORITY = 5
 KongKafkaLogHandler.VERSION = "1.0.1"
+
+local function parse_body(type, data)
+  if type and data and is_json_body(type) then
+    return cjson_decode(data)
+  end
+end
+
+function KongKafkaLogHandler:access(conf)
+  if is_json_body(kong.request.get_header("Content-Type")) then
+    local ctx = kong.ctx.plugin;
+    ctx.request_body = kong.request.get_raw_body();
+  end
+end
+
+function KongKafkaLogHandler:body_filter(conf)
+  if is_json_body(kong.response.get_header("Content-Type")) then
+    local ctx = kong.ctx.plugin;
+    local chunk, eof = ngx.arg[1], ngx.arg[2];
+    if not eof then
+      ctx.response_body = (ctx.response_body or "") .. (chunk or "")
+    end
+  end
+end
 
 --- Publishes a message to Kafka.
 -- Must run in the context of `ngx.timer.at`.
@@ -37,7 +63,13 @@ local function log(premature, conf, message)
 end
 
 function KongKafkaLogHandler:log(conf)
+
+  local ctx = kong.ctx.plugin;
   local message = basic_serializer.serialize(ngx, nil, conf)
+
+  message.request.body = parse_body(kong.request.get_header("Content-Type"), ctx.request_body)
+  message.response.body = parse_body(kong.response.get_header("Content-Type"), ctx.response_body)
+
   local ok, err = ngx.timer.at(0, log, conf, message)
   if not ok then
     kong.log.err("[kong-kafka-log] failed to create timer: ", err)
